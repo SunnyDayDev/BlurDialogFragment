8e7c2f8e695e2a38d8c3109271bede21074e168e
Tcikin Aleksandr <sunnyday.development@gmail.com>
2016-02-24 15:15:42 +0300

    Get background bitmap at onPreDraw

--- lib/src/main/java/fr/tvbarthel/lib/blurdialogfragment/BlurDialogEngine.java
+++ lib/src/main/java/fr/tvbarthel/lib/blurdialogfragment/BlurDialogEngine.java
@@ -25,6 +25,7 @@ import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.LinearInterpolator;
@@ -526,38 +527,19 @@ public class BlurDialogEngine {
         private Bitmap mBackground;
         private View mBackgroundView;
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
+        public void execute(){
             mBackgroundView = mHoldingActivity.getWindow().getDecorView();
-
-            //retrieve background view, must be achieved on ui thread since
-            //only the original thread that created a view hierarchy can touch its views.
-
-            Rect rect = new Rect();
-            mBackgroundView.getWindowVisibleDisplayFrame(rect);
-            mBackgroundView.destroyDrawingCache();
-            mBackgroundView.setDrawingCacheEnabled(true);
-            mBackgroundView.buildDrawingCache(true);
-            mBackground = mBackgroundView.getDrawingCache(true);
-
-            /**
-             * After rotation, the DecorView has no height and no width. Therefore
-             * .getDrawingCache() return null. That's why we  have to force measure and layout.
-             */
-            if (mBackground == null) {
-                mBackgroundView.measure(
-                    View.MeasureSpec.makeMeasureSpec(rect.width(), View.MeasureSpec.EXACTLY),
-                    View.MeasureSpec.makeMeasureSpec(rect.height(), View.MeasureSpec.EXACTLY)
-                );
-                mBackgroundView.layout(0, 0, mBackgroundView.getMeasuredWidth(),
-                    mBackgroundView.getMeasuredHeight());
-                mBackgroundView.destroyDrawingCache();
-                mBackgroundView.setDrawingCacheEnabled(true);
-                mBackgroundView.buildDrawingCache(true);
-                mBackground = mBackgroundView.getDrawingCache(true);
-            }
+            mBackgroundView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                @Override
+                public boolean onPreDraw() {
+                    mBackground = Bitmap.createBitmap(mBackgroundView.getWidth(), mBackgroundView.getHeight(), Bitmap.Config.ARGB_8888);
+                    Canvas canvas = new Canvas(mBackground);
+                    mBackgroundView.draw(canvas);
+                    mBackgroundView.getViewTreeObserver().removeOnPreDrawListener(this);
+                    BlurAsyncTask.super.execute();
+                    return true;
+                }
+            });
         }
 
         @Override
@@ -570,8 +552,6 @@ public class BlurDialogEngine {
             }
             //clear memory
             mBackground.recycle();
-            mBackgroundView.destroyDrawingCache();
-            mBackgroundView.setDrawingCacheEnabled(false);
             return null;
         }
 
